e18ac1e86b22a3edd25459e7c885083152f91782
Snak <72hoshino.snak@gmail.com>
2022-09-09 14:38:38 +0900

    add index view

--- hoshinonaka/urls.py
+++ hoshinonaka/urls.py
@@ -26,6 +26,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     # Uncomment the next line to enable the admin:
     path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
