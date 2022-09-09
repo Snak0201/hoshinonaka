c961c1927158844931cd8756b12dada676ca18d3
Snak <72hoshino.snak@gmail.com>
2022-09-09 13:57:00 +0900

    add blog model

--- hoshinonaka/settings.py
+++ hoshinonaka/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sites",
     "authorization.apps.AuthorizationConfig",
     "uploader.apps.UploaderConfig",
+    "blog.apps.BlogConfig",
 ]
 
 # Middleware framework
