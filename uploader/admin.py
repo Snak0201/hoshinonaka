fee53650bfdf035cc44846ef0377523d43ee2e23
Snak <72hoshino.snak@gmail.com>
2022-09-09 13:46:00 +0900

    add uploadedfile model

--- uploader/admin.py
+++ uploader/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UploadedImage
+from .models import UploadedImage, UploadedFile
 
 # Register your models here.
-admin.site.register(UploadedImage)
+admin.site.register((UploadedImage, UploadedFile))
--- uploader/models.py
+++ uploader/models.py
@@ -1,12 +1,22 @@
 from django.db import models
+import os
+import datetime
 
 # Create your models here.
-def create_media_path():
-    pass
+def generate_media_path(instance, file_name):
+    date = datetime.datetime.now().strftime("%Y/%m/%d")
+    path = os.path.join(os.path.splitext(file_name)[1], date, file_name)
+    return path
 
 
 class UploadedImage(models.Model):
-    image = models.ImageField(verbose_name="画像")
+    image = models.ImageField(verbose_name="画像", upload_to=generate_media_path)
 
     def __str__(self):
         return self.image.url
+
+class UploadedFile(models.Model):
+    file = models.FileField(verbose_name="ファイル", upload_to=generate_media_path)
+
+    def __str__(self):
+        return self.file.url
