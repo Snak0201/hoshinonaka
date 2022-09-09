121f473c64f8333499b3c384402553cc36a271f0
Snak <72hoshino.snak@gmail.com>
2022-09-09 16:54:26 +0900

    add listview

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("blog/<int:pk>/", views.ArticleView.as_view(), name="article"),
+    path("articles/", views.ArticleListView.as_view(), name="articles"),
+    path("articles/<int:article_id>/", views.ArticleView.as_view(), name="article"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, ListView
 from .models import Article
 
 # Create your views here.
@@ -6,7 +6,14 @@ class IndexView(TemplateView):
     template_name = "blog/index.html"
 
 
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = "articles"
+    template_name = "blog/articles.html"
+
+
 class ArticleView(DetailView):
     model = Article
     context_object_name = "article"
     template_name = "blog/article.html"
+    pk_url_kwarg = "article_id"
