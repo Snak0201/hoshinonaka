b11336cf62066d40112ed396034cd95194fe35c1
Snak <72hoshino.snak@gmail.com>
2022-09-09 16:10:04 +0900

    add article model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
+from distutils import extension
 from django.db import models
+from markdown import Markdown
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(verbose_name="タイトル", max_length=128)
+    markdown_text = models.TextField(verbose_name="本文（マークダウン記述）")
+    created_at = models.DateTimeField(verbose_name="作成日時", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="最終更新日時", auto_now=True)
+
+    def text(self):
+        configs = {"codehilite": {"noclasses": True}}
+
+        markdown = Markdown(
+            extensions=["extra", "fenced_code", "sane_lists"], extension_configs=configs
+        )
+        return markdown.convert(self.markdown_text)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name = "blog/index.html"
+
+
+class ArticleView(DetailView):
+    pass
