a4d8d8e40b208ac430741d561e5704c1b3ed3953
Snak <72hoshino.snak@gmail.com>
2022-09-10 22:21:12 +0900

    add uploader rename feature

--- uploader/models.py
+++ uploader/models.py
@@ -4,19 +4,24 @@ import datetime
 
 # Create your models here.
 def generate_media_path(instance, file_name):
+    if instance.name:
+        file_name = instance.name + os.path.splitext(file_name)[1]
     date = datetime.datetime.now().strftime("%Y/%m/%d")
     path = os.path.join(os.path.splitext(file_name)[1], date, file_name)
     return path
 
 
 class UploadedImage(models.Model):
-    image = models.ImageField(verbose_name="画像", upload_to=generate_media_path)
+    file = models.ImageField(verbose_name="画像", upload_to=generate_media_path)
+    name = models.CharField(verbose_name="ファイル名", max_length=128, blank=True, null=True)
 
     def __str__(self):
-        return self.image.url
+        return self.file.url
+
 
 class UploadedFile(models.Model):
     file = models.FileField(verbose_name="ファイル", upload_to=generate_media_path)
+    name = models.CharField(verbose_name="ファイル名", max_length=128, blank=True, null=True)
 
     def __str__(self):
         return self.file.url
