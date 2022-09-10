76863a220fcf04aab537b68ffa154a4155882b06
Snak <72hoshino.snak@gmail.com>
2022-09-10 22:32:23 +0900

    update file uploader

--- hoshinonaka/settings.py
+++ hoshinonaka/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sites",
-    "authorization.apps.AuthorizationConfig",
+    "django_cleanup.apps.CleanupConfig" "authorization.apps.AuthorizationConfig",
     "uploader.apps.UploaderConfig",
     "blog.apps.BlogConfig",
 ]
