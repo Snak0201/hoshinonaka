8ba370e26578e5d0f2ca0b7c776e19a3b7689bb9
Snak <72hoshino.snak@gmail.com>
2022-09-10 14:18:24 +0900

    fix committee model

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Bureau(models.Model):
     )
     name = models.CharField(verbose_name="局名", max_length=8)
     order = models.PositiveSmallIntegerField(verbose_name="配置順", default=0)
-    markdown_text = models.TextField(verbose_name="局紹介（マークダウン）")
+    markdown_text = models.TextField(verbose_name="局紹介（マークダウン）", blank=True, null=True)
 
     def text(self):
         markdown = Markdown(extensions=EXTENSIONS, extension_configs=CONFIGS)
@@ -25,37 +25,45 @@ class Bureau(models.Model):
 
 
 class Committee(models.Model):
-    code = models.SlugField(verbose_name="委員会コード", max_length=4)
+    code = models.SlugField(
+        verbose_name="委員会コード", max_length=8, primary_key=True, unique=True
+    )
     name = models.CharField(verbose_name="委員会名", max_length=32)
-    bureau = models.ForeignKey(
+    markdown_text = models.TextField(
+        verbose_name="委員会紹介（マークダウン）", blank=True, null=True
+    )
+    related_bureaus = models.ManyToManyField(
         Bureau,
-        on_delete=models.SET_NULL,
-        related_name="comittee_bureau",
-        to_field="code",
-        verbose_name="局",
-        null=True,
+        related_name="division_bureaus",
+        verbose_name="関連局",
         blank=True,
     )
-    markdown_text = models.TextField(verbose_name="委員会紹介（マークダウン）")
 
     def text(self):
         markdown = Markdown(extensions=EXTENSIONS, extension_configs=CONFIGS)
         return markdown.convert(self.markdown_text)
 
     def __str__(self):
-        return f"{self.bureau.name}: {self.name}"
+        return self.name
 
 
 class Article(models.Model):
     title = models.CharField(verbose_name="タイトル", max_length=128)
     markdown_text = models.TextField(verbose_name="本文（マークダウン）")
-    committee = models.ForeignKey(
-        Committee,
+    bureau = models.ForeignKey(
+        Bureau,
         on_delete=models.SET_NULL,
-        related_name="article_committee",
-        verbose_name="委員会",
+        related_name="article_bureau",
+        to_field="code",
+        verbose_name="局",
         blank=True,
         null=True,
+    )
+    committees = models.ManyToManyField(
+        Committee,
+        related_name="article_divisions",
+        verbose_name="委員会",
+        blank=True,
     )
     is_draft = models.BooleanField(verbose_name="下書き", default=False)
     created_at = models.DateTimeField(verbose_name="作成日時", auto_now_add=True)
