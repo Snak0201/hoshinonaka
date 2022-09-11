79bf73fe035802456e052d58996e2ae64443415f
Snak <72hoshino.snak@gmail.com>
2022-09-11 14:08:03 +0900

    add api

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,22 @@
 from dataclasses import fields
 from rest_framework.serializers import ModelSerializer
 from blog.models import Bureau
+from uploader.models import UploadedImage
 
 
 class BureauSerializer(ModelSerializer):
     class Meta:
         model = Bureau
         fields = "__all__"
+
+
+class UploadedImageSerializer(ModelSerializer):
+    class Meta:
+        model = UploadedImage
+        fields = "__all__"
+
+
+class ImageUploadSerializer(ModelSerializer):
+    class Meta:
+        model = UploadedImage
+        fields = ("file", "name")
--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,9 @@ from . import apis
 from django.urls import path
 
 urlpatterns = [
-    path("bureaus/", apis.BureauList.as_view(), name="bureaus"),
+    path("bureaus/", apis.BureauListView.as_view()),
+    path("bureaus/<slug:code>/", apis.BureauView.as_view()),
+    path("uploaders/", apis.UploadedImageListView.as_view()),
+    path("uploaders/<int:pk>/", apis.UploadedImageView.as_view()),
+    path("uploaders/upload/", apis.ImageUploadView.as_view()),
 ]
