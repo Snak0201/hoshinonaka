19d412e57c3c100c081f97ae633d5b1a0824a8a5
Snak <s.nakajima@4nonome.com>
2022-10-06 16:39:32 +0900

    remove settings

--- hoshinonaka/settings.py
+++ hoshinonaka/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django.contrib.sites",
     "django_cleanup.apps.CleanupConfig",
     "authorization.apps.AuthorizationConfig",
     "blog.apps.BlogConfig",
