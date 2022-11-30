fbb2ea3fc9128f3c64b3bfee364cdb483be49fc3
Snak <s.nakajima@4nonome.com>
2022-11-30 21:17:41 +0900

    post is not allow

--- fibonacci/apis.py
+++ fibonacci/apis.py
@@ -2,5 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class FibonacciAPIView(APIView):
+    def get_fibonacci_number(self, n):
+        return n
+    
     def get(self, request):
         return Response("OK", status=200)
+    
+    def post(self, request):
+        return Response({"status":405, "message":"Method Not Allowed."}, status=405)
