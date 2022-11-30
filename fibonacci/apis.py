990bd0d83e4b41c061849330f3124ebfab670cd3
Snak <s.nakajima@4nonome.com>
2022-11-30 21:52:59 +0900

    add validation

--- fibonacci/apis.py
+++ fibonacci/apis.py
@@ -3,14 +3,29 @@ from rest_framework.response import Response
 
 class FibonacciAPIView(APIView):
     def get_fibonacci_number(self, n):
-        if n <= 1:
+        """FIXME: この方法は遅い"""
+        if n == 0 or n == 1:
             return n
-        return self.get_fibonacci_number(n-1)+self.get_fibonacci_number(n-2)
+        return self.get_fibonacci_number(n - 1) + self.get_fibonacci_number(n - 2)
     
     def get(self, request):
-        n = int(request.GET.get("n"))
+        try:
+            n = int(request.GET.get("n"))
+            print(n)
+        except ValueError:
+            """
+            n が整数でないとき、フィボナッチ数列の定義外
+            """
+            return Response({"status":400, "message":"Request is not integer."}, status=400)
+        
+        if n < 0:
+            """
+            n が負のとき、フィボナッチ数列の定義外
+            """
+            return Response({"status":400, "message":"Request is not positive integer."}, status=400)
+        
         fn = self.get_fibonacci_number(n)
-        return Response({"result":fn}, status=200)
+        return Response({"result": fn}, status=200)
     
     def post(self, request):
         """POSTリクエストは許されない"""
