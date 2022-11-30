9bf81264593a11da4288e0d1118d487c19142d0a
Snak <s.nakajima@4nonome.com>
2022-11-30 20:33:44 +0900

    install fibonacci app

--- fibonacci/apps.py
+++ fibonacci/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class fibonacciConfig(AppConfig):
+class FibonacciConfig(AppConfig):
     name = 'fibonacci'
--- hoshinonaka/settings.py
+++ hoshinonaka/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "authorization.apps.AuthorizationConfig",
     "blog.apps.BlogConfig",
+    "fibonacci.apps.FibonacciConfig",
     "uploader.apps.UploaderConfig",
 ]
 
