72f40472adc3573f8aabdb803f7c34a21ed3eaa5
Snak <s.nakajima@4nonome.com>
2022-11-30 21:33:11 +0900

    add fibonacci method

--- fibonacci/apis.py
+++ fibonacci/apis.py
@@ -3,10 +3,15 @@ from rest_framework.response import Response
 
 class FibonacciAPIView(APIView):
     def get_fibonacci_number(self, n):
-        return n
+        if n <= 1:
+            return n
+        return self.get_fibonacci_number(n-1)+self.get_fibonacci_number(n-2)
     
     def get(self, request):
-        return Response("OK", status=200)
+        n = int(request.GET.get("n"))
+        fn = self.get_fibonacci_number(n)
+        return Response({"result":fn}, status=200)
     
     def post(self, request):
+        """POSTリクエストは許されない"""
         return Response({"status":405, "message":"Method Not Allowed."}, status=405)
